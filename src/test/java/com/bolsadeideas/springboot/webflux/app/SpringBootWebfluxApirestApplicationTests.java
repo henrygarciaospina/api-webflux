ad852ac286541537c882b72922a57870d02e5a89
henrygosling <henrygarciaospina@hotmail.com>
2022-06-23 16:38:03 -0500

    Prueba unitaria de la lista

--- src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,16 +1,33 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	public void contextLoads() {
+	public void listarTest() {
+
+		client.get()
+				.uri("/api/productos")
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+				.expectBodyList(Producto.class)
+				.hasSize(9);
+
 	}
 
 }
