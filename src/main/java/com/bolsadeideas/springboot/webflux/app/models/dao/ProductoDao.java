2b1d683b774ae5fcb43474329b534c3e93f71d5f
henrygosling <henrygarciaospina@hotmail.com>
2022-06-24 14:24:19 -0500

    Se define firma método obtenerNombre en ProductoDao mediante Query

--- src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
@@ -1,11 +1,15 @@
 package com.bolsadeideas.springboot.webflux.app.models.dao;
 
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
 public interface ProductoDao extends ReactiveMongoRepository<Producto, String>{
 
     public Mono<Producto> findByNombre(String nombre);
+
+    @Query("{ 'nombre': ?0 }")
+    public Mono<Producto> obtenerPorNombre(String nombre);
 
 }
