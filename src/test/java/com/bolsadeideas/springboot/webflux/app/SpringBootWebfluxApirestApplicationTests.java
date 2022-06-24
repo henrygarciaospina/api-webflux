aa3ba4cf9752ef032b35ea5fa8ad258fa9d5c7ef
henrygosling <henrygarciaospina@hotmail.com>
2022-06-24 14:17:43 -0500

    Se define firma método findByNombre en ProductoDao

--- src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.webflux.app;
 
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import javax.xml.transform.Source;
@@ -18,30 +20,45 @@ import java.util.List;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootWebfluxApirestApplicationTests {
 
-	@Autowired
-	private WebTestClient client;
+    @Autowired
+    private WebTestClient client;
 
-	@Test
-	public void listarTest() {
+    @Autowired
+    private ProductoService productoService;
 
-		client.get()
-				.uri("/api/productos")
-				.accept(MediaType.APPLICATION_JSON_UTF8)
-				.exchange()
-				.expectStatus().isOk()
-				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
-				.expectBodyList(Producto.class)
-				.consumeWith((response ->{
-					List<Producto> productos = response.getResponseBody();
-					productos.forEach(p ->{
-						System.out.println(p.getNombre());
-					});
 
-					Assertions.assertThat(productos.size()>0).isTrue();
+    @Test
+    public void listarTest() {
 
-				}));
-				//.hasSize(9);
+        client.get()
+                .uri("/api/productos")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Producto.class)
+                .consumeWith((response -> {
+                    List<Producto> productos = response.getResponseBody();
+                    productos.forEach(p -> {
+                        System.out.println(p.getNombre());
+                    });
 
-	}
+                    Assertions.assertThat(productos.size() > 0).isTrue();
 
+                }));
+        //.hasSize(9);
+    }
+
+    @Test
+    public void verTest() {
+
+        Mono<Producto> producto = null;
+
+        client.get()
+                .uri("/api/productos")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8);
+    }
 }
